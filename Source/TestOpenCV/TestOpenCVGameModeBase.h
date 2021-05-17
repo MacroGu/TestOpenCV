251fe111650bda9a7e6f2207ca551dfc30655033
macro gu <macrogu@qq.com>
2021-05-17 23:43:38 +0800

    添加原始工程

---
+++ Source/TestOpenCV/TestOpenCVGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestOpenCVGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTOPENCV_API ATestOpenCVGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
